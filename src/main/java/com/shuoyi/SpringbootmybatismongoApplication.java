5c02bbb37f85792ee47e71387550f01d1125f749
beallyoucanbe <shuoyi@yeah.net>
2018-01-26 21:25:02 +0800

    添加数据

--- src/main/java/com/shuoyi/SpringbootmybatismongoApplication.java
+++ src/main/java/com/shuoyi/SpringbootmybatismongoApplication.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class SpringbootmybatismongoApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootmybatismongoApplication.class, args);
 	}
--- src/main/java/com/shuoyi/repositories/Test.java
+++ src/main/java/com/shuoyi/repositories/Test.java
@@ -1,23 +1,30 @@
 package com.shuoyi.repositories;
 
+import java.io.*;
+
 /**
  * Created by zhaosy-c on 2017/9/28.
  */
 public class Test {
 
-    int a;
 
-    public Test(){
-
-        System.out.println("456");
-    }
-
-    public Test(int a){
-        this.a = a;
-
-    }
-
-    public static void my(){
+    public static void main(String[] args) throws IOException {
         System.out.println("123");
+        String path = "C:/Users/zhaosy-c/Desktop/新建文本文档.txt";
+        File file = new File(path);
+        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+
+        String line = " ";
+        int count = 0;
+        while ((line = br.readLine()) != null) {
+            String[] infos = line.split("\t");
+            for (int i = 1; i < infos.length; i++) {
+                if (Integer.parseInt(infos[i]) > 1000){
+                    System.out.println(line);
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
     }
 }
